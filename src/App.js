4bb2b6654e9606533f0941f99ab0a17b80bad397
sachin <sachinhans144@gmail.com>
2023-02-20 11:59:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Routes , Route} from "react-router-dom";
 import Home from './Home';
+
+const About = () => {
+   return (
+    <h1>About</h1>
+   )
+}
 export default function App() {
   return (
      <Routes>
         <Route path='/' element={ <Home/>}/>
+        <Route path='/' element={ <About/>}/>
      </Routes>
   )
 }
