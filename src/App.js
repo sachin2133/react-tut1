b267f09085a99e403685f9d7032083c331980718
sachin <sachinhans144@gmail.com>
2023-02-20 11:31:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes , Route} from "react-router-dom";
+import Home from './Home';
+export default function App() {
+  return (
+     <Routes>
+        <Route path='/' element={ <Home/>}/>
+     </Routes>
+  )
+}
